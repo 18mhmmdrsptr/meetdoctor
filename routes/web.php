0df72a97d40a2181a1f6d0c5199d3cfaf4d3d521
Muhammad Ari Saputra <arsamhmmd@gmail.com>
2022-09-21 16:51:33 +0800

    update login, middleware, & providers

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Frontsite\LandingController;
 use App\Http\Controllers\Frontsite\AppointmentController;
 use App\Http\Controllers\Frontsite\PaymentController;
 
+// backsite
+use App\Http\Controllers\Backsite\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,7 +32,8 @@ Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
 });
 
 Route::group(['prefix' => 'backsite', 'as' => 'backsite.', 'middleware' => ['auth:sanctum', 'verified']], function () {
-    return view('dashboard');
+    // dashboard
+    Route::resource('dashboard', DashboardController::class);
 });
 
 // Route::get('/', function () {
